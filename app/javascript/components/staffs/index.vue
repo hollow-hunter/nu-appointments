ebcbdd1b3c009f3feadd31a15b20c650a45d0792
Nelson Uribe <nelson_um@icloud.com>
2020-09-17 23:23:06 -0400

    add vue index component.

---
+++ app/javascript/components/staffs/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Staff Members</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">Staff Members</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Staff</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                    </tr>
+                  </thead>
+                  <tbody v-if="staffs">
+                    <tr v-for="staff in staffs" :key="staff.id">
+                      <td>{{staff.id}}</td>
+                      <td>{{staff.name}}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data: function(){
+    return {
+      staffs: null
+    }
+  },
+  mounted(){
+    axios.get('/api/staffs')
+      .then(response => {
+        this.staffs = response.data;
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }
+}
+</script>
