8badb1ed39d767d6277f766eac4060bbc8bad64a
Nelson Uribe <nelson_um@icloud.com>
2020-09-19 20:47:25 -0400

    add creation staff action.

--- app/controllers/api/staffs_controller.rb
+++ app/controllers/api/staffs_controller.rb
@@ -4,5 +4,20 @@ module Api
       all = Staff.all
       render json: all
     end
+
+    def create
+      staff = Staff.new(staff_params)
+      if staff.save
+        render json: staff, status: :created
+      else
+        render json: staff.errors, status: :bad_request
+      end
+    end
+
+    private
+
+    def staff_params
+      params.permit(:name)
+    end
   end
 end
---
+++ app/javascript/components/staffs/new.vue
@@ -0,0 +1,81 @@
+<template>
+<div>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>New Staff</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item active">New Staff</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="col-md-6 col-sm-12">
+        <!-- general form elements -->
+        <div class="card card-primary">
+          <div class="card-header">
+            <h3 class="card-title">Staff</h3>
+          </div>
+          <!-- /.card-header -->
+          <!-- form start -->
+          <form v-on:submit.prevent="saveStaff" role="form">
+            <div class="card-body">
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input v-model="name" required type="text" class="form-control" id="name" placeholder="Name">
+              </div>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+              <button type="submit" title="save" class="btn btn-primary">
+                <i class="fa fa-save"></i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+</template>
+<script>
+import axios from 'axios'
+import toastr from 'toastr'
+export default {
+  data: function() {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    saveStaff: function() {
+      axios
+        .post('/api/staffs', {
+            name: this.name
+        })
+        .then(response => {
+          toastr.success('Staff created.');
+          window.location.href = '/staffs';
+        })
+        .catch(error => {
+          toastr.error("Staff couldn't be created.");
+          console.log(error.response);
+        });
+    }
+  }
+}
+</script>
