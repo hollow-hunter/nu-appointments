6527ff0b5dc6825d500942aad4800f0ff7af80d1
Nelson Uribe <nelson_um@icloud.com>
2020-09-24 17:55:54 -0400

    move stylesheets to webpack folder.

--- app/webpack/packs/application.js
+++ app/webpack/packs/application.js
@@ -16,6 +16,7 @@ window.$ = window.jQuery = jQuery;
 
 require("bootstrap");
 require("admin-lte");
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
