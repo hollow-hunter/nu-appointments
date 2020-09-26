17cdc7c4d54e038339a5b40069a3593d2686fffd
Nelson Uribe <nelson_um@icloud.com>
2020-09-25 23:34:51 -0400

    add api edit actions.

--- app/controllers/api/staffs_controller.rb
+++ app/controllers/api/staffs_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class StaffsController < ActionController::API
+    before_action :set_staff, only: %i[show update]
+
     def index
       all = Staff.all
       render json: all
@@ -14,7 +16,23 @@ module Api
       end
     end
 
+    def show
+      render json: @staff, status: :ok
+    end
+
+    def update
+      if @staff.update(staff_params)
+        render json: @staff, status: :ok
+      else
+        render json: @staff.errors, status: :bad_request
+      end
+    end
+
     private
+
+    def set_staff
+      @staff = Staff.find(params[:id])
+    end
 
     def staff_params
       params.permit(:name)
