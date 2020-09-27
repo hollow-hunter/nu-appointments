89c289c19d5b4e1eeb7af1ace48d0f334e22efe0
Nelson Uribe <nelson_um@icloud.com>
2020-09-27 15:07:01 -0400

    add edit staff link.

--- app/webpack/components/staffs/index.vue
+++ app/webpack/components/staffs/index.vue
@@ -1,77 +1,83 @@
 <template>
-  <div>
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Staff Members</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/">Home</a></li>
-              <li class="breadcrumb-item active">Staff Members</li>
-            </ol>
-          </div>
+<div>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Staff Members</h1>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item active">Staff Members</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Staff</h3>
-                <div class="card-tools">
-                  <div class="input-group input-group-sm">
-                    <div class="input-group-append">
-                      <a href="/staffs/new" title="Create new" class="btn btn-default">
-                        <i class="fas fa-plus"></i>
-                      </a>
-                    </div>
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Staff</h3>
+              <div class="card-tools">
+                <div class="input-group input-group-sm">
+                  <div class="input-group-append">
+                    <a href="/staffs/new" title="Create new" class="btn btn-default">
+                      <i class="fas fa-plus"></i>
+                    </a>
                   </div>
                 </div>
               </div>
-              <!-- /.card-header -->
-              <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Name</th>
-                    </tr>
-                  </thead>
-                  <tbody v-if="staffs">
-                    <tr v-for="staff in staffs" :key="staff.id">
-                      <td>{{staff.id}}</td>
-                      <td>{{staff.name}}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
             </div>
-            <!-- /.card -->
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0">
+              <table class="table table-hover text-nowrap">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody v-if="staffs">
+                  <tr v-for="staff in staffs" :key="staff.id">
+                    <td>{{staff.id}}</td>
+                    <td>{{staff.name}}</td>
+                    <td>
+                      <a target="_blank" :href="editUrl(staff.id)">
+                        <i class="fa fa-edit"></i>
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
           </div>
+          <!-- /.card -->
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
 </template>
 <script>
 import axios from 'axios'
 export default {
-  data: function(){
+  data: function() {
     return {
       staffs: null
     }
   },
-  mounted(){
+  mounted() {
     axios.get('/api/staffs')
       .then(response => {
         this.staffs = response.data;
@@ -79,6 +85,11 @@ export default {
       .catch(error => {
         console.log(error.response);
       });
+  },
+  methods: {
+    editUrl: function(id) {
+      return '/staffs/' + id + '/edit';
+    }
   }
 }
 </script>
