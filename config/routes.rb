77befda41efea02b3616e444c363c24688e3eeb8
Nelson Uribe <nelson_um@icloud.com>
2020-09-30 22:49:16 -0400

    add clients api controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :staffs
   namespace :api do
     resources :staffs
+    resources :clients
   end
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
