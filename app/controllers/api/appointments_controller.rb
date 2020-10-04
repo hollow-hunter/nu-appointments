9d8f066f485ce6f8bae15d80b72b3ca2676ffd05
Nelson Uribe <nelson_um@icloud.com>
2020-10-04 17:34:08 -0400

    add appointments api controller.

---
+++ app/controllers/api/appointments_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  class AppointmentsController < ActionController::API
+    before_action :set_appointment, only: %i[edit update show]
+
+    def index
+      render json: Appointment.all, status: :ok
+    end
+
+    def create
+      a = Appointment.new(appointment_params)
+      if a.save
+        render json: a, status: :created, location: api_appointment_path(a)
+      else
+        render json: a.errors, status: :bad_request
+      end
+    end
+
+    def update
+      if @appointment.update(appointment_params)
+        render json: :show, status: :ok, location: api_appointment_path(@appointment)
+      else
+        render json: @appointment.errors, status: :bad_request
+      end
+    end
+
+    def show
+      render json: @appointment, status: :ok
+    end
+
+    private
+
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
+    end
+
+    def appointment_params
+      params.permit(:date, :start_time, :end_time, :client_id, :staff_id, :status)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :staffs
     resources :clients
+    resources :appointments
   end
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
