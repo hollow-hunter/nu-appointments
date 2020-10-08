abce12c442107174acef85842ae486b35548e95d
Nelson Uribe <nelson_um@icloud.com>
2020-10-07 23:45:05 -0400

    add calendar view for appointments.

---
+++ app/webpack/packs/appointments/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue/dist/vue.esm.js'
+import '../../stylesheets/appointments.scss';
+import Index from '../../components/appointments/index.vue'
+
+new Vue({
+  el: '#app',
+  components: { Index }
+})
