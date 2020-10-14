d759ab6fbdc3020d62f3fbef7bba1ed1396c24c7
Nelson Uribe <nelson_um@icloud.com>
2020-10-13 23:02:40 -0400

    add devise user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    get '/register', to: 'devise/registrations#new'
+  end
   resources :staffs
   resources :clients
   resources :appointments
