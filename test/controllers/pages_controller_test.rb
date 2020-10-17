bb4bfbd6064135ae5f3f0672b17b448271ebf21c
Nelson Uribe <nelson_um@icloud.com>
2020-10-17 19:39:41 -0400

    add devise helpers.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index as root" do
+    sign_in users(:one)
     get root_path
     assert_response :success
   end
--- test/controllers/staffs_controller_test.rb
+++ test/controllers/staffs_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class StaffsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:one)
     get staffs_path
     assert_response :success
   end
