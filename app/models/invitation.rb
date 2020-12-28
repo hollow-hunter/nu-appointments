c6762e30448e2ddb9f3b92f270ab3b3767393fcb
Nelson Uribe <nelson_um@icloud.com>
2020-12-28 18:30:37 -0400

    add code generation for invitation.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,18 @@
+require 'securerandom'
+
 class Invitation < ApplicationRecord
   belongs_to :company
 
   before_save :set_expiration
+  before_create :generate_code
 
   private
 
   def set_expiration
     self.expires_in = 24.hours.from_now
+  end
+
+  def generate_code
+    self.code = SecureRandom.hex
   end
 end
--- test/models/invitation_test.rb
+++ test/models/invitation_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class InvitationTest < ActiveSupport::TestCase
-  test 'invitation is saved with expiration in 24 hours' do
+  test 'is saved with expiration in 24 hours' do
     i = Invitation.new company_id: companies(:company_one).id
     assert i.save, "invitation wasn't saved"
     assert i.expires_in <= 24.hours.from_now, 'invitation expiration is more than 24 hours'
+  end
+
+  test 'code is generated automatically' do
+    i = Invitation.new company_id: companies(:company_one).id
+    assert i.save, "invitation wasn't saved"
+    assert_not_nil i.code, 'invitation code was not generated'
+  end
+
+  test 'code is different for each invitation' do
+    i = Invitation.new company_id: companies(:company_one).id
+    i2 = Invitation.new company_id: companies(:company_one).id
+    assert i.save, "invitation wasn't saved"
+    assert i2.save, "invitation wasn't saved"
+    assert_not_equal i.code, i2.code, 'codes were the same'
   end
 end
