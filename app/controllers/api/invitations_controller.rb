8833bb79c265339014f6140f2d3ef4c383004e59
Nelson Uribe <nelson_um@icloud.com>
2021-01-03 19:08:55 -0400

    send email with invitation for registration.

--- app/controllers/api/invitations_controller.rb
+++ app/controllers/api/invitations_controller.rb
@@ -4,6 +4,7 @@ module Api
       i = Invitation.new(invitation_params)
       i.company_id = current_user.company_id
       if i.save
+        InvitationMailer.invitation_created(i.email, i.code, i.company.name).deliver_later
         render json: { id: i.id }, status: :created
       else
         render json: i.errors, status: :bad_request
