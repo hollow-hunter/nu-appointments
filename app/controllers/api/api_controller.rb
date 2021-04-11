9c7a16ddc1903da39ae7b1533ba69924873dfcd9
Nelson Uribe <nelson_um@icloud.com>
2021-04-11 19:47:09 -0400

    add base API controller with authorization checking.

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  class ApiController < ActionController::API
+    protected
+
+    def check_authorization!
+      doorkeeper_authorize! if current_user.nil?
+    end
+
+    def current_user
+      @devise_current_user ||= warden.authenticate(scope: :user)
+      return @devise_current_user unless @devise_current_user.nil?
+
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
+  end
+end
